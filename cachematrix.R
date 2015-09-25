d53c022ffe3def6491928f0c5d814163e5f306df
luckyseedling <amandalittler@me.com>
2015-09-25 13:36:46 +0100

    1st attempt at cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions below work together to store an inverted matrix in a cache and to fetch the data back when required
 
-## Write a short comment describing this function
+## To make use of the functions you can run:-
+## 
+## source("cachematrix.R")    
+## x <- makeCacheMatrix()     
+## x$set_matrix(matrix(rnorm(100),10,10))   
+## cacheSolve(x)  - this 1st run returns an inverted matrix from working environment
+##
+## cacheSolve(x)  - this 2nd and further runs returns and inverted matrix from the cache along with a print statement
+
+## makeCacheMatrix is a function which creates 4 smaller functions which can be used to store and fetch data from a cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        cache <- NULL 
+        ##sets cache to NULL initially
+        
+        set_matrix <- function(y) {
+                x <<- y
+                cache <<- NULL
+        }
+        ##recreates the matrix in the fuction environment
+        
+        get <- function() x
+        ##function to get the matrix values
+        
+        store_inverted <- function(solve) cache <<- solve
+        ##function to store the inverted values in the cache
+        
+        get_inverted_from_cache <- function() cache
+        ##function to return values from the cache
+        
+        list(set_matrix = set_matrix, get = get, store_inverted = store_inverted, get_inverted_from_cache = get_inverted_from_cache)
+        ##return functions to environment outside of the makeCacheMatrix functions
+        
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is a function which goes on to make use of the 4 functions created within makeCacheMatrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { 
+        
+        cache<- x$get_inverted_from_cache()
+        ##populates cache if any data is available
+        
+        if(!is.null(cache)) { 
+                message("data fetched from cache")
+                return(cache)
+        }
+        ##evaluates cache and returns data from it if cache is not NULL
+        ##also prints a messahe to inform user that data is from cache
+        
+        else
+                data <- x$get()
+        cache<- solve(data, ...)
+        x$store_inverted(cache)
+        
+        cache
+        ##if there is no data in cache when the cacheSolve function is called then it is stored
+        
 }
